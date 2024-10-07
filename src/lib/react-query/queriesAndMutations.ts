f64d876195b57a9bb7feea9e20ddd0b1c49c3010
Dhruvi Patel <dp7070dp@gmail.com>
2024-10-06 22:59:00 -0400

    completed all users, liked posts, saved post, user profile

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,5 +1,11 @@
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from "@tanstack/react-query";
-import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts, signInAccount, signOutAccount, updatePost } from "../appwrite/api";
+import {
+    createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser,
+    getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts,
+    signInAccount, signOutAccount, updatePost,
+    getUsers,
+    getUserById
+} from "../appwrite/api";
 import { INewPost, INewUser, IUpdatePost } from "@/types";
 import { QUERY_KEYS } from "./queryKeys";
 
@@ -130,8 +136,6 @@ export const useGetPostById = (postId?: string) => {
         queryFn: () => getPostById(postId),
         enabled: !!postId, //if postid is same then do not fetch again
     });
-
-
 }
 
 export const useUpdatePost = () => {
@@ -161,17 +165,17 @@ export const useDeletePost = () => {
 
 export const useGetPosts = () => {
     return useInfiniteQuery({
-      queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-      queryFn: getInfinitePosts as any,
-      getNextPageParam: (lastPage: any) => {
-        if (!lastPage || lastPage.documents.length === 0) {
-          return null;
-        }
-    
-        const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
-        return lastId;
-      },
-      initialPageParam: null, // Set initialPageParam to null
+        queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+        queryFn: getInfinitePosts as any,
+        getNextPageParam: (lastPage: any) => {
+            if (!lastPage || lastPage.documents.length === 0) {
+                return null;
+            }
+
+            const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+            return lastId;
+        },
+        initialPageParam: null, // Set initialPageParam to null
     });
 }
 
@@ -181,4 +185,23 @@ export const useSearchPost = (searchTerm: string) => {
         queryFn: () => searchPosts(searchTerm),
         enabled: !!searchTerm
     })
+}
+
+
+export const useGetUsers = (limit?: number) => {
+    return useQuery(
+        {
+            queryKey: [QUERY_KEYS.GET_USERS],
+            queryFn: () => getUsers(limit)
+        })
+}
+
+
+export const useGetPostUserById = (userId?: string) => {
+
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_USER_BY_ID, userId],
+        queryFn: () => getUserById(userId),
+        enabled: !!userId, //if postid is same then do not fetch again
+    });
 }
